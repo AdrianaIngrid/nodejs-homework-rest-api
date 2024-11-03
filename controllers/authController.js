6c9b46df7cfc92ed47adaf28f21626a2836138b6
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-11-03 16:53:53 +0200

    end

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,9 @@
 const services = require("../services/index");
 const jwt = require("jsonwebtoken");
+const Jimp = require("jimp");
+const fs = require("fs").promises;
+const path = require("path");
+const avatarsDir = path.join(__dirname, "../public/avatars");
 require("dotenv").config();
 const secret = process.env.SECRET;
 const User = require("../services/schemas/userSchema");
@@ -77,10 +81,36 @@ const currentUserController = (req, res) => {
     subscription,
   });
 };
+const uploadAvatarController = async (req, res, next) => {
+   
+
+    try {
+        if (!req.file) {
+            return res.status(404).json({ error: "There is no file to upload" });
+                
+        }
+        const { path: tmpPath, originalname } = req.file;
+        const avatarName = `${req.user._id}-${Date.now() + path.extname(originalname)
+            }`;
+        const finalAvatarPath = path.join(avatarsDir, avatarName);
+        const image = await Jimp.read(tmpPath);
+        await image.resize(250, 250).writeAsync(finalAvatarPath);
+        await fs.unlink(tmpPath);
+        const avatarURL = `/public/avatars/${avatarName}`;
+        await User.findByIdAndUpdate(req.user._id, { avatarURL }, { new: true });
+
+        res.status(200).json({ avatarURL });
+    } catch (error) {
+        next(error);
+    }
+};
+
 
 module.exports = {
   createUserController,
   loginUserController,
   logoutController,
-  currentUserController,
+    currentUserController,
+    uploadAvatarController,
+
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const tmpDir = path.join(__dirname, "../tmp");
+
 const {
   createUserController,
   loginUserController,
     logoutController,
   currentUserController,
+  uploadAvatarController,
 } = require("../controllers/authController");
 const validateSignUp = require("../middlewares/validateSignUp");
 const loginValidate = require("../middlewares/validateLogin");
-const {auth } = require("../middlewares/auth");
+const { auth } = require("../middlewares/auth");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, tmpDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = `${Date.now()}+${file.originalname}`;
+    cb(null, uniqueName);
+  },
+});
 
-
+const upload = multer({ storage: storage });
 
 router.post("/signup", validateSignUp, createUserController);
 router.post("/login", loginValidate, loginUserController);
 router.get("/logout", auth, logoutController);
 router.get("/current", auth, currentUserController);
+router.patch("/avatars", auth, upload.single("avatar"), uploadAvatarController);
 module.exports = router;
 
